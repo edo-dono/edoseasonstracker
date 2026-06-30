a8455a6991df269a11aeb772f27a088919db9a2a
edo-dono <122275682+edo-dono@users.noreply.github.com>
2026-06-30 10:14:55 +0200

    v0.4

---
+++ sw.js
@@ -0,0 +1,38 @@
+/* edoSeasonsTracker — service worker (cache hors-ligne) */
+const CACHE = 'edoseasonstracker-v0.4';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-512-maskable.png',
+  './favicon.png',
+  'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(r =>
+      r || fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy).catch(() => {}));
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
